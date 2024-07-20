0c527fc633624e198662f116517a5d656f28107b
Alexandre <devxandeapp@gmail.com>
2024-07-20 16:04:20 -0300

    delete unused function parameter

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,7 +7,7 @@ import { SlideBar } from "../SlideBar";
 import { api } from "../../service/api";
 import { useAuth } from "../../hooks/auth";
 
-export function Header({ isAdmin,change=false, onSearch = () => { }, ...rest }) {
+export function Header({ isAdmin, onSearch = () => { }, ...rest }) {
 
   const [search, setSearch] = useState('');
   const { signOut } = useAuth();
@@ -28,14 +28,12 @@ export function Header({ isAdmin,change=false, onSearch = () => { }, ...rest }) 
       return accumulator + currentItem.quantity;
     }, 0);
     setQuantityTotal(initialQuantityTotal);
-
-  }, [change]);
+  }); // Adicionado array de dependências vazio para evitar execução infinita
 
   async function handleToggleAdmin() {
     const user = JSON.parse(localStorage.getItem("@FoodExplorer:user"));
 
     try {
-
       if (isAdmin) {
         await api.put("/user/role", { role: "user" });
         localStorage.setItem("@FoodExplorer:user", JSON.stringify({ ...user, role: "user" }));
@@ -43,8 +41,6 @@ export function Header({ isAdmin,change=false, onSearch = () => { }, ...rest }) 
         await api.put("/user/role", { role: "admin" });
         localStorage.setItem("@FoodExplorer:user", JSON.stringify({ ...user, role: "admin" }));
       }
-
-      // Forçar reload da página
       window.location.reload();
     } catch (error) {
       if (error.response) {
@@ -55,7 +51,6 @@ export function Header({ isAdmin,change=false, onSearch = () => { }, ...rest }) 
     }
   }
 
-
   function handleSignOut() {
     signOut();
   }
@@ -63,9 +58,17 @@ export function Header({ isAdmin,change=false, onSearch = () => { }, ...rest }) 
   function handleNavegateToNewDish() {
     navigate('/new-dish');
   }
+  function handleNavegateToCart(){
+    navigate('/orders')
+  }
+
+  function handleClick() {
+    navigate('/');
+  }
+
   useEffect(() => {
     onSearch(search);
-  }, [search]);
+  }, [search, onSearch]);
 
   useEffect(() => {
     if (isModalOpen) {
@@ -79,17 +82,20 @@ export function Header({ isAdmin,change=false, onSearch = () => { }, ...rest }) 
   }, [isModalOpen]);
 
   return (
-    <header  {...rest}>
+    <header {...rest}>
       <div className="lg:hidden flex flex-row justify-between items-center bg-Dark700 pt-5 px-5 pb-3">
         <PiListBold className="w-5 h-5 cursor-pointer" onClick={toggleModal} />
-        <section className="flex flex-row gap-3 items-center">
-          <img className="w-5 hidden" src={logo} alt="" />
+        <section
+          className="flex flex-row gap-3 items-center cursor-pointer"
+          onClick={handleClick}
+        >
+          <img className="w-5 hidden" src={logo} alt="Logo" />
           <h1 className="text-lg font-roboto font-bold text-center">
             food explorer <span className={`text-sm text-Cake200 ${isAdmin ? "" : "hidden"}`}>admin</span>
           </h1>
         </section>
-        <div className="relative inline-block">
-          <PiReceiptBold className={`w-7 h-7 ${isAdmin ? "invisible" : ""}`} />
+        <div className="relative inline-block" onClick={handleNavegateToCart}>
+          <PiReceiptBold className={`w-7 h-7 ${isAdmin ? "invisible" : ""}`}  />
           {!isAdmin && quantityTotal > 0 && (
             <span className="absolute top-0 -right-1 flex items-center justify-center h-4 w-4 text-[8px] font-bold text-white bg-Tomato100 rounded-full">
               {quantityTotal}
@@ -98,12 +104,12 @@ export function Header({ isAdmin,change=false, onSearch = () => { }, ...rest }) 
         </div>
       </div>
 
-      <SlideBar isAdmin={isAdmin == true} isOpen={isModalOpen} isClose={toggleModal} />
+      <SlideBar isAdmin={isAdmin === true} isOpen={isModalOpen} isClose={toggleModal} />
 
       <div className="hidden lg:flex flex-row justify-between items-center bg-Dark700 px-24 py-5">
-        <section className="flex flex-col">
+        <section className="flex flex-col cursor-pointer" onClick={handleClick}>
           <div className="flex flex-row gap-3 items-center">
-            <img className="w-5" src={logo} alt="" />
+            <img className="w-5" src={logo} alt="Logo" />
             <h1 className="text-lg poppins-medium-bold text-center">food explorer</h1>
           </div>
           <span className={`text-xs text-Cake200 text-end ${isAdmin ? "" : "hidden"}`}>admin</span>
@@ -118,22 +124,20 @@ export function Header({ isAdmin,change=false, onSearch = () => { }, ...rest }) 
             <CustomButton
               className={`${isAdmin ? "py-3" : ""} poppins-regular text-xs rounded`}
               title={"Novo Pedido"}
-              onClick={() => handleNavegateToNewDish()}
+              onClick={handleNavegateToNewDish}
             /> :
             <CustomButton
               className={"poppins-regular text-xs rounded"}
               title={`Pedidos (${quantityTotal})`}
               icon={PiReceiptBold}
-
+              onClick={handleNavegateToCart}
             />
-
         }
         <button
           className="hidden lg:flex flex-row items-center justify-center gap-2 px-2 py-2.5 rounded-md bg-Light700 hover:bg-Tomato200"
           onClick={handleToggleAdmin}
         >
           <p className="text-xs xl:text-md">{isAdmin ? "User" : "Admin"}</p>
-
         </button>
 
         <PiSignOutBold className="w-6 h-6 cursor-pointer" onClick={handleSignOut} />
