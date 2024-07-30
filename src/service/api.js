4fab1878c38e007c9a1de9f91a7a1fd8ec87a64e
Alexandre <devxandeapp@gmail.com>
2024-07-30 09:48:05 -0300

    adjust link for prod

--- src/service/api.js
+++ src/service/api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 export const api = axios.create({
-  baseURL:"http://localhost:3333",
+  baseURL:"https://backendfoodexplore.onrender.com",
 });
 
 api.interceptors.response.use(
